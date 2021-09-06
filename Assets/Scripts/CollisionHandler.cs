02fe84b8ebde145fbb479af6fa3c6235b96544ea
Joseph <JRSiwiecki@gmail.com>
2021-09-06 16:25:01 -0700

    Implemented reloading the level when the rocket crashes.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -15,8 +16,14 @@ public class CollisionHandler : MonoBehaviour
                 break;
             
             default:
-                Debug.Log("Bumped into an obstacle!");
+                ReloadLevel();
                 break;
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
